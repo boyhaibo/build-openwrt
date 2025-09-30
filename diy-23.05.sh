6f531fb9b727169c1f6ca25fe4ca3a2083232b79
haiibo <boyhaibo@gmail.com>
2025-09-30 21:38:23 +0800

    update 2025-09-30 21:38:22

--- diy-23.05.sh
+++ diy-23.05.sh
@@ -2,10 +2,12 @@
 
 color() {
     case $1 in
-        cr) echo -e "\e[1;31m$2\e[0m" ;;
-        cg) echo -e "\e[1;32m$2\e[0m" ;;
-        cy) echo -e "\e[1;33m$2\e[0m" ;;
-        cb) echo -e "\e[1;34m$2\e[0m" ;;
+        cr) echo -e "\e[1;31m$2\e[0m" ;;  # 红色
+        cg) echo -e "\e[1;32m$2\e[0m" ;;  # 绿色
+        cy) echo -e "\e[1;33m$2\e[0m" ;;  # 黄色
+        cb) echo -e "\e[1;34m$2\e[0m" ;;  # 蓝色
+        cp) echo -e "\e[1;35m$2\e[0m" ;;  # 紫色
+        cc) echo -e "\e[1;36m$2\e[0m" ;;  # 青色
     esac
 }
 
@@ -170,7 +172,7 @@ sed -i "s|firmware_repo.*|firmware_repo 'https://github.com/$GITHUB_REPOSITORY'|
 # sed -i "s|kernel_path.*|kernel_path 'https://github.com/ophub/kernel'|g" $destination_dir/luci-app-amlogic/root/etc/config/amlogic
 sed -i "s|ARMv8|$RELEASE_TAG|g" $destination_dir/luci-app-amlogic/root/etc/config/amlogic
 
-# 开始加载个人设置
+# 加载个人设置
 begin_time=$(date '+%H:%M:%S')
 
 # 修改默认IP
@@ -192,7 +194,7 @@ cp -f $GITHUB_WORKSPACE/images/bg1.jpg feeds/luci/themes/luci-theme-argon/htdocs
 sed -i "s/DISTRIB_REVISION=.*/DISTRIB_REVISION=''/g" package/base-files/files/etc/openwrt_release
 sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='%D R$(date +%y.%-m.%-d)'/g" package/base-files/files/etc/openwrt_release
 
-# 取消主题默认设置
+# 删除主题默认设置
 # find $destination_dir/luci-theme-*/ -type f -name '*luci-theme-*' -print -exec sed -i '/set luci.main.mediaurlbase/d' {} \;
 
 # 调整 netdata 到 状态 菜单
@@ -221,12 +223,12 @@ for e in $(ls -d $destination_dir/luci-*/po feeds/luci/applications/luci-*/po); 
 done
 status "加载个人设置"
 
-# 开始更新配置文件
+# 更新配置文件
 begin_time=$(date '+%H:%M:%S')
 make defconfig 1>/dev/null 2>&1
 status "更新配置文件"
 
-# 开始下载openclash运行内核
+# 下载openclash运行内核
 [ $CLASH_KERNEL ] && grep -q "luci-app-openclash=y" .config && {
     begin_time=$(date '+%H:%M:%S')
     chmod +x $GITHUB_WORKSPACE/scripts/preset-clash-core.sh
@@ -234,7 +236,7 @@ status "更新配置文件"
     status "下载openclash运行内核"
 }
 
-# 开始下载adguardhome运行内核
+# 下载adguardhome运行内核
 [ $CLASH_KERNEL ] && grep -q "luci-app-adguardhome=y" .config && {
     begin_time=$(date '+%H:%M:%S')
     chmod +x $GITHUB_WORKSPACE/scripts/preset-adguard-core.sh
@@ -242,7 +244,7 @@ status "更新配置文件"
     status "下载adguardhome运行内核"
 }
 
-# 开始下载zsh终端工具
+# 下载zsh终端工具
 grep -q "zsh=y" .config &&  {
     begin_time=$(date '+%H:%M:%S')
     chmod +x $GITHUB_WORKSPACE/scripts/preset-terminal-tools.sh
